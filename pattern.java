ffe2adae6db3e71200e38527830e7a6ec58c89dd
bapu-1777 <mayankpv2000@gmail.com>
2022-03-07 18:52:05 +0530

    pattern 22

--- pattern.java
+++ pattern.java
@@ -1,8 +1,9 @@
+import javax.crypto.spec.RC2ParameterSpec;
 
 public class pattern{
     public static void main(String[] args) {
 
-        pattern12(5);
+        pattern23(3);
         
     }
     public static void pattern1(int n){
@@ -149,6 +150,247 @@ public class pattern{
 
     }
 
+    public static void pattern13(int n){
 
+        for(int row =1; row<=n; row++){
+            int logic = n-row;
+            if(0!=logic){
+                System.out.printf("%"+logic+"s"," ");
+            } 
+            for(int col=1; col<=row*2-1; col++){
+                
+                if(col==1 || col==row*2-1 || row==n){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                } 
+            }
+            System.out.println();
+        }
 
-}   
+    }
+    public static void pattern14(int n){
+        
+        for(int row =1; row<=n; row++){
+            int logic = row-1;
+            if(0!=logic){
+                System.out.printf("%"+logic+"s"," ");
+            }
+            
+            for(int col=1; col<=(n-row)*2+1; col++){
+                if ( row==1 || col==1 || col==(n-row)*2+1){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+    public static void pattern15(int n){
+
+        for(int row =1; row<=n*2-1; row++){
+            int logic = row>n ? n*2-row : row;
+            int space = n-logic;
+            if (0!=space){
+                System.out.printf("%"+space+"s"," ");
+            }
+            for(int col=1; col<=logic*2-1; col++){
+                if (col==1 || col == logic*2-1){
+                    System.out.print("*");
+                }else{
+                System.out.print(" ");
+            }
+        }
+            System.out.println();
+        }
+
+    }
+    //https://www.geeksforgeeks.org/pascal-triangle/
+    public static void pattern16(int n){
+
+        for(int row =1; row<=n; row++){
+            int space = n-row;
+            if (0!=space){
+                System.out.printf("%"+space+"s"," ");
+            }
+            int logic = 1;
+            for (int col=1;col <=row;col++){
+                System.out.print(logic+" ");
+                logic = logic*(row-col)/col;
+                // System.out.print(logic);
+            }
+            System.out.println();
+        }
+
+    }
+    //      1
+    //     212
+    //    32123
+    //   4321234
+    //    32123
+    //     212
+    //      1
+
+    public static void pattern17(int n){
+
+        for(int row =1; row<=n*2-1; row++){
+            int logic = row>n ? n*2-row : row;
+            int space=n-logic;
+            if(0!=space){
+                System.out.printf("%"+space+"s"," ");
+            }
+            for(int col1=logic; col1>=1; col1--){
+                System.out.print(col1);
+            }
+            for(int col2=2;col2<=logic;col2++){
+                System.out.print(col2);
+            }
+            System.out.println();
+        }
+
+    }
+
+    //   **********
+    //   ****  ****
+    //   ***    ***
+    //   **      **
+    //   *        *
+    //   *        *
+    //   **      **
+    //   ***    ***
+    //   ****  ****
+    //   **********
+    public static void pattern18(int n){
+
+        for(int row =1; row<=n*2; row++){
+            int logic = row<=5 ? n-row+1:row-n;
+            for(int col=1; col<=n*2; col++){ 
+                if (col<=logic || n*2-logic<col){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }            
+            }
+            System.out.println();
+        }
+
+    }
+    //    *        *
+    //    **      **
+    //    ***    ***
+    //    ****  ****
+    //    **********
+    //    ****  ****
+    //    ***    ***
+    //    **      **
+    //    *        *
+
+    public static void pattern19(int n){
+
+        for(int row =1; row<=n*2-1; row++){
+            int logic = row>5 ? n*2-row : row;
+            for(int col=1; col<=n*2; col++){
+                if(col<=logic || n*2-logic<col){
+
+                System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+        }
+
+    }
+    public static void pattern20(int n){
+
+        for(int row =1; row<=n; row++){
+            for(int col=1; col<=n; col++){
+                if(row==1 || row==n || col==1 || col ==n){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+    public static void pattern21(int n){
+        int logic=1;
+        for(int row =1; row<=n; row++){
+            
+            for(int col=1; col<=row; col++){
+                System.out.print(logic+" ");
+                logic++;
+            }
+            System.out.println();
+        }
+
+    }
+//      *****
+//     *   *
+//    *   *
+//   *   *
+//  *****
+    public static void pattern25(int n){
+
+        for(int row =1; row<=n; row++){
+            int space = n-row;
+            if (0!=space){
+                System.out.printf("%"+space+"s"," ");
+            }
+            for(int col=1; col<=n; col++){
+                if (row==1 || row == n || col==1 || col == n){
+                    System.out.print("*");    
+                }
+                else{
+                    System.out.print(" "); 
+                }
+                
+            }
+            System.out.println();
+        }
+    }
+    //    1
+    //    0 1
+    //    1 0 1
+    //    0 1 0 1
+    //    1 0 1 0 1
+    public static void pattern22(int n){
+        int num= 1;
+        for(int row =1; row<=n; row++){
+            for(int col=1; col<=row; col++){
+                System.out.print(num+" ");
+                num = num==1 ? 0 :1;
+            }
+            System.out.println();
+        }
+
+    }
+//     *   *
+//    * * * *
+//   *   *   *
+public static void pattern23(int n){
+    
+    for(int row =1; row<=n; row++){
+        int space = n-row;
+        if (0!=space){
+            System.out.printf("%"+space+"s"," ");
+        }
+        for(int col=1; col<=n; col++){
+            System.out.print("*");
+            
+        }
+        System.out.println();
+    }
+
+}
+}
